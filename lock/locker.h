ddbacf3e4fa42f5f68a580ebce53b50b3eb0be83
Seren1ty23 <luyunhao0510@gmail.com>
2026-09-12 15:18:16 +0800

    stage2: 进程/线程同步封装

---
+++ lock/locker.h
@@ -0,0 +1,8 @@
+//
+// Created by Lu Yunhao on 2026/9/12.
+//
+
+#ifndef TINYWEBSERVER_LOCK_H
+#define TINYWEBSERVER_LOCK_H
+
+#endif //TINYWEBSERVER_LOCK_H
---
+++ test/lock_test.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Lu Yunhao on 2026/9/12.
+//
